6bbbfc21d12dac4d90d0c15715cc37aad3d3c26b
Nova Cypher <novacypher@oortech.com>
2025-03-13 23:16:22 -0400

    chore: update

--- deploy/DeployAdapter.ts
+++ deploy/DeployAdapter.ts
@@ -4,10 +4,10 @@ import { type DeployFunction } from 'hardhat-deploy/types'
 import { getDeploymentAddressAndAbi } from '@layerzerolabs/lz-evm-sdk-v2'
 import { getNamedAccounts } from 'hardhat'
 
-const contractName = 'OORTOFTUpgradeableTest'
+const contractName = 'OORTOFTUpgradeable'
 const chain = {
-    tokenAddress: '0x4872d996f8a94A044AE8e1fa1d928a431602D3b5',
-    endpointAddress: '0x6EDCE65403992e310A62460808c4b910D972f10f',
+    tokenAddress: '0x5651fA7a726B9Ec0cAd00Ee140179912B6E73599',
+    endpointAddress: '0x1a44076050125825900e736c501f859c50fE728c', //Same endpoint address for current networks
 }
 
 const deploy: DeployFunction = async (hre) => {
@@ -18,7 +18,7 @@ const deploy: DeployFunction = async (hre) => {
     const { address, abi } = getDeploymentAddressAndAbi(hre.network.name, 'EndpointV2')
     //const endpointV2Deployment = new Contract(address, abi, signer)
     try {
-        const proxy = await hre.ethers.getContract('OORTOFTUpgradeableTest')
+        const proxy = await hre.ethers.getContract('OORTOFTUpgradeable')
         console.log(`Proxy: ${proxy.address}`)
     } catch (e) {
         console.log(`Proxy not found`)
@@ -29,7 +29,7 @@ const deploy: DeployFunction = async (hre) => {
         args: [chain.tokenAddress, chain.endpointAddress], // replace '0x' with the address of the ERC-20 token
         log: true,
         waitConfirmations: 1,
-        skipIfAlreadyDeployed: false,
+        skipIfAlreadyDeployed: true,
         proxy: {
             owner: deployer,
             proxyContract: 'UUPS',
